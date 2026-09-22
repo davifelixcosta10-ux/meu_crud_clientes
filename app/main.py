84ad45472a76a51b90cc6aa32b8011e7ff6d7d8d
Davi Felix Costa <davifelixcosta10@gmail.com>
2026-09-22 15:28:11 -0300

    fix(import): JSONResponse from starlette.responses not fastapi

--- app/main.py
+++ app/main.py
@@ -22,7 +22,8 @@ Segurança:
 
 import os
 from datetime import date
-from fastapi import FastAPI, HTTPException, status, Header, Depends, Request, JSONResponse
+from fastapi import FastAPI, HTTPException, status, Header, Depends, Request
+from starlette.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
